2bee339cbe7b7cf5cc6e81a817970c49638cbe5d
jpsant <j.paaulinho@gmail.com>
2020-07-25 23:27:57 -0300

    removed unused codes and created a global project stylesheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ApplicationContainer from './containers/ApplicationContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      <ApplicationContainer />
     </div>
   );
 }
